41c62982f4bc314fa48337ed2808d46e4a87ec71
n1klasD <67094918+n1klasD@users.noreply.github.com>
2021-06-03 16:02:47 +0200

    added docstrings

--- games/Pong.py
+++ games/Pong.py
@@ -1,3 +1,7 @@
+"""
+This File contains the classes Player, Ball, and the main class Pong, which is used to start  the pong game
+"""
+
 import pygame
 
 from util import Game, Image
@@ -7,26 +11,47 @@ import numpy as np
 
 
 class Player(Image):
+    """
+    Child class of the Image class. Represents the player in a pong game.
+    Apart from handling the image of the players, it also handles his movement.
+    The flags move_up or move_down can be set and by calling the
+    move()- function, the player position is updated.
+    """
+
     def __init__(self, x, y):
         self.player_size = (20, 150)
         super().__init__(x, y, self.player_size, "plane.png")
 
-        # movement flags
+        # movement flags --> These determine in which direction the player is moved,
+        # when move() is called
+
         self.move_up = False
         self.move_down = False
 
     def move(self):
-        speed = 0
+        """
+        This function handles the movement of the player.
+        When move_up or move_down is set, the Y- coordinate of the player is adjusted in either direction.
+        The movement is not updated, if the player comes close to the boundaries of the window to prevent
+        him from exiting the window.
+        """
+
+        speed = 10
         if self.move_up:
             if self.getY() > 20:
-                speed = -10
+                self.setY(self.getY() - speed)
         if self.move_down:
             if self.getY() < (Configuration.windowHeight - 20 - self.player_size[1]):
-                speed = 10
-        self.setY(self.getY() + speed)
+                self.setY(self.getY() + speed)
 
 
 class Ball(Image):
+    """
+    This class is a child class of the Image class. It represents the pong ball.
+    Apart from handling the image of the ball, it also handles his movement, collision,
+    and the the check, if a player has scored a point.
+    """
+
     def __init__(self, x, y):
         self.ball_size = (30, 30)
         super().__init__(x, y, self.ball_size, "ball.png")
@@ -38,26 +63,69 @@ class Ball(Image):
         self.last_flip = (x, y)  # is in the middle of the screen when initializing
 
     def didCollideWithPlayer(self, player: Player):
+        """
+        This function checks if the ball collides with a given player.
+
+        Args:
+            player (Player): the player used to check for collision
+
+        Return:
+            None
+        """
+
         ball_rect = self.getRect()
         return ball_rect.colliderect(player.getRect())
 
     def did_collide_top_bottom(self):
+        """
+        This function checks if the ball collides with the top or the bottom of the window.
+        It returns a Boolean if a collision happens. This is used for the wall collision.
+
+        Return:
+            Boolean
+        """
+
         y_coord = self.getY()
         return y_coord < 0 or (y_coord + self.ball_size[1]) > Configuration.windowHeight
 
     def flip_velocity(self, mode="x"):
+        """
+        This function flips the velocity vector of the ball.
+
+        Args:
+            mode (String): The axis, on which the velocity vector is flipped. When mode is x, it flips the velocity
+            on the x- axis (1,1) -> ( 1,-1). When mode is y, it flips the velocity on the y- axis (1,1) -> (-1, 1).
+
+        Return:
+            None
+        """
+
         if mode == "x":
             self.speed = (-self.speed[0], self.speed[1])
         elif mode == "y":
             self.speed = (self.speed[0], -self.speed[1])
 
     def move(self):
+        """
+        This function moves the ball by it's speed vector.
+
+        Return:
+            None
+        """
         new_x = self.getX() + self.speed[0]
         new_y = self.getY() + self.speed[1]
         self.setX(new_x)
         self.setY(new_y)
 
     def determine_winner(self):
+        """
+        Determines, if a player has won the game. If the ball moves out of the vertical boundary of the window,
+        the player on the opposite side scores a goal.
+
+        Return:
+            Integer (1 if p1 scores, 2 if player 2 scores)
+        """
+
         if self.getX() + self.SIZE[0] < 0:
             # point for player two
             return 2
@@ -66,11 +134,25 @@ class Ball(Image):
             return 1
 
     def reset(self):
+        """
+        Resets the the balls speed to a random velocity and places it in the middle of the screen.
+
+        Return:
+            None
+        """
+
         self.speed = self.getRandomVelocity()
         self.setX(Configuration.windowWidth / 2)
         self.setY(Configuration.windowHeight / 2)
 
     def getRandomVelocity(self):
+        """
+        Generates a random velocity tuple, that has random x/y values and signs.
+
+        Return:
+            (Integer, Integer)
+        """
+
         v_x, v_y = randint(5, 15), randint(5, 7)
         pos_or_negativ = np.random.randint(2, size=2)
 
@@ -84,6 +166,12 @@ class Ball(Image):
 
 
 class Pong(Game):
+    """
+    This class is a child class of the Game class. Apart from the parent functions it handles the general gameloop
+    of the pong game, instantiates the players and the ball, handles key events and the scoring system. Furthermore it
+    creates the score counters and handles the general screen updates.
+    """
+
     def __init__(self):
         super().__init__()
 
@@ -100,8 +188,9 @@ class Pong(Game):
         self._score = [0, 0]
         self._font = pygame.font.Font('freesansbold.ttf', 32)
 
-        # calculate number of spacers
+        # calculate the number of spacers by windowheight
         number_of_spacers = Configuration.windowHeight // 40
+        # create an Array of Image objects to be displayed on the screen
         self.spacers = list()
         for i in range(number_of_spacers):
             self.spacers.append(Image(Configuration.windowWidth / 2, 40 * i, (10, 30), "spacer.png"))
@@ -110,14 +199,34 @@ class Pong(Game):
         self.run()
 
     def updateScore(self, player: int):
+        """
+        This function updates the score counter and increase the points of one player.
+
+        Args:
+            player (Integer): The player whose score will be increased by one (1 --> Player one, 2 --> Player two)
+
+        Return:
+            None
+        """
+
         if player == 1:
             self._score[0] += 1
         elif player == 2:
             self._score[1] += 1
 
-    def handleEvent(self, event):
-        # event handling
+    def handleEvent(self, event) -> None:
+        """
+        This function overrides the handleEvent function of it´s parent class. It handles all keyboard events
+        that are necessary for updating the players movement and sets their movement flags accordingly.
 
+        Args:
+            event (pygame.event.Event): The to be handled event
+
+        Return:
+            None
+        """
+
+        # event handling
         # Handle keydown events
         if event.type == pygame.KEYDOWN:
 
@@ -152,9 +261,16 @@ class Pong(Game):
             if event.key == pygame.K_RIGHT:
                 self.player_two.move_down = False
 
-    def updateGameState(self):
-        # borders, movement handling
+    def updateGameState(self) -> None:
+        """
+        This method handles all game-specific state changes. Furthermore it moves the players and the ball, handles
+        their collision, determines if a player has scored a point and increases their score.
 
+        Return:
+            None
+        """
+
+        # borders, movement handling
         # movement updates
         self.ball.move()
         self.player_one.move()
@@ -180,6 +296,10 @@ class Pong(Game):
             self.ball.reset()
 
     def updateScreen(self):
+        """
+        This method updates the pygame window by drawing object. Here it draws the ball and the player,
+        as well as the spacers in the middle of the game and the score counters
+        """
         # fill game display black
         self.surface.fill((0, 0, 0))
 
