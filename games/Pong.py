cd170c7afade23d0a55f3fa1b266ec8b220a3cd7
n1klasD <67094918+n1klasD@users.noreply.github.com>
2021-06-03 11:34:36 +0200

    Added random ball speed generation and reset after one player wins

--- games/Pong.py
+++ games/Pong.py
@@ -2,12 +2,13 @@ import pygame
 
 from util import Game, Image
 from config import Configuration
+from random import randint
 
 
 class Player(Image):
     def __init__(self, x, y):
-        player_size = (20, 100)
-        super().__init__(x, y, player_size, "plane.png")
+        self.player_size = (20, 150)
+        super().__init__(x, y, self.player_size, "plane.png")
 
         # movement flags
         self.move_up = False
@@ -19,18 +20,21 @@ class Player(Image):
             if self.getY() > 20:
                 speed = -10
         if self.move_down:
-            if self.getY() < (Configuration.windowHeight - 20 - self.SIZE[1]):
+            if self.getY() < (Configuration.windowHeight - 20 - self.player_size[1]):
                 speed = 10
         self.setY(self.getY() + speed)
 
 
 class Ball(Image):
     def __init__(self, x, y):
-        ball_size = (20, 20)
-        super().__init__(x, y, ball_size, "ball.png")
+        self.ball_size = (30, 30)
+        super().__init__(x, y, self.ball_size, "ball.png")
 
         # speed
-        self.speed = (5, 2)
+        self.speed = (randint(10, 20), randint(5, 10))
+
+        # last velocity flip
+        self.last_flip = (x, y)  # is in the middle of the screen when initializing
 
     def didCollideWithPlayer(self, player: Player):
         ball_rect = self.getRect()
@@ -38,10 +42,9 @@ class Ball(Image):
 
     def did_collide_top_bottom(self):
         y_coord = self.getY()
-        return y_coord < 0 or (y_coord + self.SIZE[1]) > Configuration.windowHeight
+        return y_coord < 0 or (y_coord + self.ball_size[1]) > Configuration.windowHeight
 
-    def flipVelocity(self, mode="x"):
-        # can only flip velocity again after it travelled for 20 pixels
+    def flip_velocity(self, mode="x"):
         if mode == "x":
             self.speed = (-self.speed[0], self.speed[1])
         elif mode == "y":
@@ -53,11 +56,23 @@ class Ball(Image):
         self.setX(new_x)
         self.setY(new_y)
 
+    def determine_winner(self):
+        if self.getX() + self.SIZE[0] < 0:
+            # point for player two
+            return 2
+        elif self.getX() > Configuration.windowWidth:
+            # point for player one
+            return 1
+
+    def reset(self):
+        self.speed = (randint(10, 20), randint(5, 10))
+        self.setX(Configuration.windowWidth / 2)
+        self.setY(Configuration.windowHeight / 2)
+
 
 class Pong(Game):
     def __init__(self):
         super().__init__()
-        print("Bing Pong du hurnson")
 
         # 2 Players / 1 Player selection Menu
 
@@ -66,7 +81,20 @@ class Pong(Game):
         self.player_two = Player(Configuration.windowWidth - 100, 50)
         self.ball = Ball(Configuration.windowWidth / 2, Configuration.windowHeight / 2)
 
+        # counter
+        self._counter = (0, 0)
+        self._font = pygame.font.Font('freesansbold.ttf', 32)
+
+        # calculate number of spacers
+        number_of_spacers = (Configuration.windowHeight) // 40
+        self.spacers = list()
+        for i in range(number_of_spacers):
+            self.spacers.append(Image(Configuration.windowWidth / 2, 40 * i, (10, 30), "spacer.png"))
+
         self.run()
+
+    def updateCounter(self):
+        return self._font.render(f"{self._counter[0]}:{self._counter[1]}", True, (255, 255, 255))
 
     def handleEvent(self, event):
         # event handling
@@ -115,10 +143,19 @@ class Pong(Game):
 
         # collision handling
         if self.ball.didCollideWithPlayer(self.player_one) or self.ball.didCollideWithPlayer(self.player_two):
-            self.ball.flipVelocity(mode="x")
+            self.ball.flip_velocity(mode="x")
 
         if self.ball.did_collide_top_bottom():
-            self.ball.flipVelocity(mode="y")  # flip velocity vector on y-axis
+            self.ball.flip_velocity(mode="y")  # flip velocity vector on y-axis
+
+        # determine winner
+        winner = self.ball.determine_winner()
+        if winner == 1:
+            # increase player one score
+            self.ball.reset()
+        elif winner == 2:
+            # increase player two score
+            self.ball.reset()
 
     def updateScreen(self):
         # fill game display black
@@ -128,7 +165,9 @@ class Pong(Game):
         self.drawImageOnSurface(self.player_one)
         self.drawImageOnSurface(self.player_two)
         self.drawImageOnSurface(self.ball)
+        for image in self.spacers:
+            self.drawImageOnSurface(image)
 
-        # draw players and ball
+        self.drawTextOnSurface((255,255,255), "Hi", (0,0))
 
         super().updateScreen()
