ec371676f43ed83a4bc1cb8ab645b2b2d8362996
Simon <stauss.simon.01@gmail.com>
2021-06-05 12:43:11 +0200

    added basic score handling

--- games/TicTacToe.py
+++ games/TicTacToe.py
@@ -1,9 +1,13 @@
+from config import Configuration
+from pandas import DataFrame
 from util import Game
 
 
 class TicTacToe(Game):
     def __init__(self):
-        super().__init__()
+        super().__init__(game="TicTacToe")
         print("XXX - i won")
 
+        self.isGameOver = True
+        self.isRunning = False
         self.run()
